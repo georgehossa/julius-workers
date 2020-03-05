1956a13220a44da3d0855e26ba458676e6ac5490
Jorge Ossa <jorgehossa@gmail.com>
2020-03-05 09:09:48 -0500

    Config handleDelete

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Axios from 'axios';
+import axios from 'axios';
 
 const Employee = (props) => {
   const { id, name, age, salary, avatar } = props;
-  const deleteEmploye = () => {
-    Axios.delete(`https://dummy.restapiexample.com/api/v1/delete/${id}`)
+  const handleDeleteEmploye = () => {
+    axios.delete(`http://localhost:5000/employees/${id}`)
       .then(res => console.log(res))
       .catch(error => console.log(error))
   }
@@ -21,7 +21,7 @@ const Employee = (props) => {
           <button className="rounded p-1 text-gray-500 hover:text-blue-400 transition-colors duration-300" type="button">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path className="fill-current heroicon-ui" d="M6.3 12.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z"/></svg>
           </button>
-          <button onClick={deleteEmploye} className="rounded p-1 text-gray-500 hover:text-red-400 transition-colors duration-300" type="button">
+          <button onClick={handleDeleteEmploye} className="rounded p-1 text-gray-500 hover:text-red-400 transition-colors duration-300" type="button">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path className="fill-current heroicon-ui" d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8H3a1 1 0 1 1 0-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1z"/></svg>
           </button>
         </div>
