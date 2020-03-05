f66e7de1244f82f7c52b7a2ce940dfd45b72ff6b
Jorge Ossa <jorgehossa@gmail.com>
2020-03-05 11:52:09 -0500

    Configure component to create a new Employee

--- src/containers/NewEmployee.jsx
+++ src/containers/NewEmployee.jsx
@@ -1,25 +1,54 @@
 import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
-const NewEmployee = () => {
+const NewEmployee = ({history}) => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [salary, setSalary] = useState(0);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+  const handleAge = (e) => {
+    setAge(e.target.value);
+  }
+  const handleSalary = (e) => {
+    setSalary(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      name: name,
+      age: age,
+      salary: salary,
+      avatar: `https://api.adorable.io/avatars/190/${encodeURI(name.toLocaleLowerCase())}.png`
+    }
+    Axios.post('http://localhost:5000/employees/add', newUser)
+    .then(res => console.log(res.data))
+    .catch(error => console.log(error));
+    // window.location.replace('/');
+    history.push('/');
+  }
   return (
     <div className="container mx-auto">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full" onSubmit={handleSubmit}>
         <div className="mb-4">
           <label htmlFor="name" className="block">Name:</label>
-          <input type="text" name="name" placeholder="Name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
+          <input onChange={handleName} type="text" name="name" placeholder="Name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
           <p className="text-red-500 text-xs italic mt-2 invisible">Please place a Name</p>
         </div>
         <div className="mb-4">
           <label htmlFor="age" className="block">Age:</label>
-          <input type="text" name="age" placeholder="Age" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
+          <input onChange={handleAge} type="text" name="age" placeholder="Age" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
           <p className="text-red-500 text-xs italic mt-2 invisible">Please place a Employee Age</p>
         </div>
         <div className="mb-4">
           <label htmlFor="salary" className="block">Salary:</label>
-          <input type="text" name="salary" placeholder="Salary" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
+          <input onChange={handleSalary} type="text" name="salary" placeholder="Salary" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
           <p className="text-red-500 text-xs italic mt-2 invisible">Please place a Salary</p>
         </div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
           Create
         </button>
 
